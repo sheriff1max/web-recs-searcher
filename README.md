44a808ebe25676cec3a60d80eca899e7b89a2a90
morph1max <kobelevmaxim48@gmail.com>
2023-12-20 22:34:04 +0700

    connect module recs-searcher to backend

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # web-recs-searcher
 
-> Docker:
-    cd fastapi-vue
-    docker-compose up -d --build
+Два способа запуска на выбор:
+1. Docker:
+```
+cd fastapi-vue
+docker-compose up -d --build
+```
 
-> Local run:
-    cd fastapi-vue/services/backend
-    uvicorn src.main:app --reload --host 0.0.0.0 --port 5000
-
-    cd fastapi-vue/services/frontend
-    npm run serve
+2. Local run:
+> Backend:
+```
+cd fastapi-vue/services/backend
+uvicorn src.main:app --reload --host 0.0.0.0 --port 5000
+```
+> Frontend:
+```
+cd fastapi-vue/services/frontend
+npm run serve
+```
--- fastapi-vue/services/backend/src/main.py
+++ fastapi-vue/services/backend/src/main.py
@@ -8,6 +8,8 @@ from .utils import parse_pipeline_name
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from recs_searcher import load_pipeline
+
 
 app = FastAPI()
 app.add_middleware(
@@ -18,31 +20,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.PATH_WEIGHTS = os.path.join('src', 'models', 'weights')
+app.MODEL_NAME = 'Города России.pkl'
+app.MODEL = load_pipeline(app.PATH_WEIGHTS, app.MODEL_NAME)
 
 @app.get('/')
 def home():
     return {
-        'list_task_names': os.listdir('src/pipelines'),
+        'list_task_names': os.listdir(app.PATH_WEIGHTS),
     }
 
 
 @app.post('/')
 def predict(params_for_pipeline: ParamsForPipeline):
 
-    return {
-        'result': [
-            {'text': 'Iphone 11', 'similarity': 0.55},
-            {'text': 'Iphone 12', 'similarity': 0.74},
-        ]
-    }
+    # return {
+    #     'result': [
+    #         {'text': 'Iphone 11', 'similarity': 0.55},
+    #         {'text': 'Iphone 12', 'similarity': 0.74},
+    #     ]
+    # }
+    if params_for_pipeline.task_name == app.MODEL_NAME:
+        pass
+    else:
+        app.MODEL = load_pipeline(app.PATH_WEIGHTS, params_for_pipeline.task_name)
 
-    df_pred = ...  # pd.DataFrame
-    text_array = df_pred.text.values
+    df_pred = app.MODEL.search(params_for_pipeline.text, params_for_pipeline.k)  # pd.DataFrame
+    text_array = df_pred.name.values
     similarity_array = df_pred.similarity.values
 
     result_list = []
     for i in range(len(text_array)):
-        tmp_dict = {'text': text_array[i], 'similarity': similarity_array[i]}
+        tmp_dict = {
+            'text': text_array[i],
+            'similarity': float(similarity_array[i])
+        }
         result_list.append(tmp_dict)
-
     return {'result': result_list}
