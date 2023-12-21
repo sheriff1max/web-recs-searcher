0fea7be7fea9a841a5273a94db00336f0e3d2189
Maxim Kobelev <kobelevmaxim48@gmail.com>
2023-12-21 14:55:45 +0700

    update docker

--- fastapi-vue/services/backend/src/models/train.py
+++ fastapi-vue/services/backend/src/models/train.py
@@ -10,17 +10,17 @@ import pandas as pd
 
 
 SEED = 1
-PIPELINE_NAME = 'Города России'
+PIPELINE_NAME = 'Компьютерные игры'
 
 
 if __name__ == '__main__':
-    # dataset_games = pd.read_csv('./datasets/video_games.csv')
+    dataset_games = pd.read_csv('./datasets/video_games.csv')
     # dataset_exoplanets = pd.read_csv('./datasets/exoplanets.csv')
-    dataset_city_russia = pd.read_csv('./datasets/city_russia.csv')
+    # dataset_city_russia = pd.read_csv('./datasets/city_russia.csv')
     # dataset_games = dataset.load_video_games()
     # dataset_exoplanets = dataset.load_exoplanes()
     # dataset_city_russia = dataset.load_city_russia()
-    dataset = dataset_city_russia
+    dataset = dataset_games
 
     preprocessing_list = [
         preprocessing.BaseCleaner(remove_number=False),
@@ -32,40 +32,40 @@ if __name__ == '__main__':
         ),
     ]
 
-    # model_fasttext = models.FastTextWrapperModel(
-    #     min_count=1,
-    #     vector_size=200,
-    #     window=2,
-    #     sg=1,
-    #     hs=1,
-    #     epochs=70,
-    #     min_n=0,
-    #     seed=SEED,
-    # )
-
-    augmentation_transforms_seed_none = [
-        augmentation.MisspellingAugmentation(
-            add_syms={'p': 0.01, 'language': 'russian'},
-            change_syms={'p': 0.01, 'language': 'russian'},
-            delete_syms={'p': 0.01},
-            multiply_syms={'p': 0.01},
-            swap_syms={'p': 0.01},
-            seed=None,
-        ),
-    ]
-    model_transformer = models.SentenceTransformerWrapperModel(
-        augmentation_transform=augmentation_transforms_seed_none,
-        batch_size=32,
-        epochs=5,
-        optimizer_params={'lr': 2e-2},
+    model_fasttext = models.FastTextWrapperModel(
+        min_count=1,
+        vector_size=200,
+        window=2,
+        sg=1,
+        hs=1,
+        epochs=70,
+        min_n=0,
+        seed=SEED,
     )
+
+    # augmentation_transforms_seed_none = [
+    #     augmentation.MisspellingAugmentation(
+    #         add_syms={'p': 0.01, 'language': 'russian'},
+    #         change_syms={'p': 0.01, 'language': 'russian'},
+    #         delete_syms={'p': 0.01},
+    #         multiply_syms={'p': 0.01},
+    #         swap_syms={'p': 0.01},
+    #         seed=None,
+    #     ),
+    # ]
+    # model_transformer = models.SentenceTransformerWrapperModel(
+    #     augmentation_transform=augmentation_transforms_seed_none,
+    #     batch_size=32,
+    #     epochs=5,
+    #     optimizer_params={'lr': 2e-2},
+    # )
 
     searcher_faiss = similarity_search.FaissSearch
 
     pipeline = api.Pipeline(
         dataset=dataset.target.values,
         preprocessing=preprocessing_list,
-        model=model_transformer,
+        model=model_fasttext,
         searcher=searcher_faiss,
         verbose=True,
     )
